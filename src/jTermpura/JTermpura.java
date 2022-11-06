cff120542d69571672176ea0ee9847d0c9335d6f
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2022-11-05 20:29:16 -0400

    I have no idea wat i am doing

--- src/jTermpura/JTermpura.java
+++ src/jTermpura/JTermpura.java
@@ -17,15 +17,36 @@ public class JTermpura {
 			+ " ░███    ░███ ░███  ███░░███  ░███ ░███  ░███░░███ \r\n"
 			+ " ███████████  █████░░████████ ████ █████ ████ █████\r\n"
 			+ "░░░░░░░░░░░  ░░░░░  ░░░░░░░░ ░░░░ ░░░░░ ░░░░ ░░░░░ ";
+	
+	public final static String description = "Create delicious Terminal UIs without the culinary classes.\r\n"
+			+ "\r\n"
+			+ "With Termpura you can craft up an interface in your terminal using a quick-and-simple framework built with developers in mind. Termpura improves productivity and allows anyone to get off on a running start on their next great project.\r\n"
+			+ "\r\n"
+			+ "With Termpura on Replit you can start coding in an amazing IDE with all the features you need setup! Just start typing away and hit that run button.\r\n"
+			+ "\r\n"
+			+ "Termpura features:\r\n"
+			+ "\r\n"
+			+ " - File-based pages and routing.\r\n"
+			+ " - Functional handler and router system.\r\n"
+			+ " - Programmable middleware that can be used for authentication, displays, and more.\r\n"
+			+ " - Intuitive, dev-friendly environment (especially for those who are familiar with\r\n"
+			+ " frameworks like NextJS).\r\n"
+			+ " - and more! 🎉";
 
 	// TODO complete basics of main page
 	public static void mainPage() {
 		System.out.println(logo);
 		System.out.println("\n\n");
+		System.out.println(description);
 	}
 	
 	// TODO complete 404 pages
 	public static void notFound404Error(String notFoundLogo) {
+		
+	}
+	
+	// TODO comple 500 pages
+	public static void internalServer505Error(String internalServerErrLogo) {
 		
 	}
 
